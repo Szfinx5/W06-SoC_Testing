c985f50a67fdbe09bb8ea6e80e3924198a1352a9
ghavasi <g.havasi@outlook.com>
2022-06-04 19:00:16 +0100

    scrabble-score can handle multipe letters and words

--- w6d2_test-driven-development-mino-gabor/scrabble-score.js
+++ w6d2_test-driven-development-mino-gabor/scrabble-score.js
@@ -1,1 +1,46 @@
-export function calculateScrabbleScore(word) {}
+export function calculateScrabbleScore(word) {
+  let wordArray = word.toUpperCase().split("");
+  let counter = 0;
+
+  for (let i = 0; i < wordArray.length; i++) {
+    if (
+      wordArray[i] === "A" ||
+      wordArray[i] === "E" ||
+      wordArray[i] === "I" ||
+      wordArray[i] === "O" ||
+      wordArray[i] === "U" ||
+      wordArray[i] === "L" ||
+      wordArray[i] === "N" ||
+      wordArray[i] === "R" ||
+      wordArray[i] === "S" ||
+      wordArray[i] === "T"
+    )
+      counter = counter + 1;
+
+    if (wordArray[i] === "D" || wordArray[i] === "G") counter = counter + 2;
+
+    if (
+      wordArray[i] === "B" ||
+      wordArray[i] === "C" ||
+      wordArray[i] === "M" ||
+      wordArray[i] === "P"
+    )
+      counter = counter + 3;
+
+    if (
+      wordArray[i] === "F" ||
+      wordArray[i] === "H" ||
+      wordArray[i] === "V" ||
+      wordArray[i] === "W" ||
+      wordArray[i] === "Y"
+    )
+      counter += 4;
+
+    if (wordArray[i] === "K") counter += 5;
+
+    if (wordArray[i] === "J" || wordArray[i] === "X") counter += 8;
+
+    if (wordArray[i] === "Q" || wordArray[i] === "Z") counter += 10;
+  }
+  return counter;
+}
--- w6d2_test-driven-development-mino-gabor/scrabble-score.test.js
+++ w6d2_test-driven-development-mino-gabor/scrabble-score.test.js
@@ -2,3 +2,100 @@ import { calculateScrabbleScore } from "./scrabble-score";
 
 // List of matchers (expects) for jest:
 // https://jestjs.io/docs/using-matchers
+
+test("Test A letter", () => {
+  const actual = calculateScrabbleScore("A");
+  const expedted = 1;
+
+  expect(actual).toBe(expedted);
+});
+
+let scoreOneArray = ["E", "I", "O", "U", "L", "N", "R", "S", "T"];
+let scoreTwoArray = ["D", "G"];
+let scoreThreeArray = ["B", "C", "M", "P"];
+let scoreFourArray = ["F", "H", "V", "W", "Y"];
+let scoreFiveArray = ["K"];
+let scoreEightArray = ["J", "X"];
+let scoreTenArray = ["Q", "Z"];
+
+for (let i = 0; i < scoreOneArray.length; i++) {
+  test("testing for " + scoreOneArray[i], () => {
+    const actual = calculateScrabbleScore(scoreOneArray[i]);
+    const expedted = 1;
+    expect(actual).toBe(expedted);
+  });
+}
+
+for (let i = 0; i < scoreTwoArray.length; i++) {
+  test("testing for " + scoreTwoArray[i], () => {
+    const actual = calculateScrabbleScore(scoreTwoArray[i]);
+    const expedted = 2;
+    expect(actual).toBe(expedted);
+  });
+}
+
+for (let i = 0; i < scoreThreeArray.length; i++) {
+  test("testing for " + scoreThreeArray[i], () => {
+    const actual = calculateScrabbleScore(scoreThreeArray[i]);
+    const expedted = 3;
+    expect(actual).toBe(expedted);
+  });
+}
+
+for (let i = 0; i < scoreFourArray.length; i++) {
+  test("testing for " + scoreFourArray[i], () => {
+    const actual = calculateScrabbleScore(scoreFourArray[i]);
+    const expedted = 4;
+    expect(actual).toBe(expedted);
+  });
+}
+
+for (let i = 0; i < scoreFiveArray.length; i++) {
+  test("testing for " + scoreFiveArray[i], () => {
+    const actual = calculateScrabbleScore(scoreFiveArray[i]);
+    const expedted = 5;
+    expect(actual).toBe(expedted);
+  });
+}
+
+for (let i = 0; i < scoreEightArray.length; i++) {
+  test("testing for " + scoreEightArray[i], () => {
+    const actual = calculateScrabbleScore(scoreEightArray[i]);
+    const expedted = 8;
+    expect(actual).toBe(expedted);
+  });
+}
+
+for (let i = 0; i < scoreTenArray.length; i++) {
+  test("testing for " + scoreTenArray[i], () => {
+    const actual = calculateScrabbleScore(scoreTenArray[i]);
+    const expedted = 10;
+    expect(actual).toBe(expedted);
+  });
+}
+
+test("Testing for 'MARYLAND'.", () => {
+  const actual = calculateScrabbleScore("MARYLAND");
+  const expected = 14;
+  expect(actual).toBe(expected);
+});
+
+test("Testing for 'pinakanakakapagngitngitngitngitang'.", () => {
+  const actual = calculateScrabbleScore("pinakanakakapagngitngitngitngitang");
+  const expected = 56;
+  expect(actual).toBe(expected);
+});
+
+test("Testing for 'Megszentsegtelenithetetlensegeskedeseitekert'.", () => {
+  const actual = calculateScrabbleScore(
+    "Megszentsegtelenithetetlensegeskedeseitekert"
+  );
+  const expected = 70;
+  expect(actual).toBe(expected);
+});
+
+test("Testing for 'Kraftfahrzeughaftpflichtversicherung'.", () => {
+  const actual = calculateScrabbleScore("Kraftfahrzeughaftpflichtversicherung");
+  const expected = 84;
+  expect(actual).toBe(expected);
+});
